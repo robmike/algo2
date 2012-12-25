6278861eaaf48aef10f8ee6f942c49ead31d5afa
invalid <invalid@invalid>
2012-12-24 22:18:44 -0500

    hw2: q2 (correct but 1 day late)

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -76,6 +76,82 @@ def cluster(nclusters = 4):
             print "error"
         return partitions, separation
 
-print "foo"
-p = cluster(2)
-print p[1]
+
+# q2
+def bin(s):
+   return str(s) if s<=1 else bin(s>>1) + str(s&1)
+
+class Counter(object):
+   def __init__(self, d, nbits):
+      self.d = d
+      self.nbits = nbits
+
+def nextPairWithDist(nodes, d, nbits):
+   # next pair with distance d
+   if(len(nodes) == 1):
+      raise(StopIteration)
+   for n in nodes:
+      for i in xrange(0, nbits - d + 1):
+         # solution only handles d = 2
+         # can handle arbitrary d with
+         # base-nbits d digit adder
+         if d == 2:
+            for j in xrange(i+1, nbits):
+               candidate = n ^ (1 << i) ^ (1 << j)
+               # print i,j, bin(candidate)
+               if candidate in nodes and candidate > n:
+                  yield d, n, candidate
+         else:
+            candidate = n ^ (1 << i)
+            # print bin(candidate)
+            if candidate in nodes and candidate > n:
+               yield d, n, candidate
+   raise(StopIteration)
+
+   
+
+def hammingcluster():
+    h = []
+    partitions = DisjointSet()
+    maxdistance = 2
+    nodes = set([])
+    with open('clustering2.txt', 'r') as f:
+        nvert_donotuse, nbits = [int(x) for x in f.readline().split()]
+        # edges = recfromtxt(f, dtype=int, usemask=False)
+        for line in f:
+            nodeint = int(''.join([str(x) for x in line.split()]), 2)
+            nodes.add(nodeint)
+            partitions.makeset(nodeint)
+
+        nvert = len(nodes)
+
+        for d in xrange(1,maxdistance+1):
+           for nextpair in nextPairWithDist(nodes, d, nbits):
+              ecost, u, v = nextpair
+              if not partitions.sameset(u,v):
+                 partitions.merge(u,v)
+
+        return partitions.count() #, partitions.partitions
+
+# unused function
+def binsearchmincluster():
+   l, r = 0, 20000
+   i  = r
+   while(l < r):
+      print i, l, r
+      i = (l + r)/2
+      success = hammingcluster(i)
+      if not success:
+         r = i
+      else:
+         l = i + 1
+   return i - 1     # Returns largest cluster size that meets criteria
+
+
+# print "q1"
+# p = cluster(4)
+# # print p[0].partitions
+# print p[1]
+
+print "q2"
+# p = hammingcluster(2)
