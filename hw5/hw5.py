8444bd32be71c304f27cf71679b6bdc048af11f7
invalid <invalid@invalid>
2013-01-15 17:50:54 -0500

    hw5: wip

---
+++ hw5/hw5.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+import pdb, sys
+# debug shit (from stackexchange)
+def info(type, value, tb):
+   if hasattr(sys, 'ps1') or not sys.stderr.isatty():
+      # we are in interactive mode or we don't have a tty-like
+      # device, so we call the default hook
+      sys.__excepthook__(type, value, tb)
+   else:
+      import traceback, pdb
+      # we are NOT in interactive mode, print the exception...
+      traceback.print_exception(type, value, tb)
+      print
+      # ...then start the debugger in post-mortem mode.
+      pdb.pm()
+
+sys.excepthook = info
+
+def distance(pos1, pos2):
+    return np.sqrt(np.sum(np.square(pos1 - pos2)))
+
+# Calculate pairwise distances via outer-product np
+# broadcasting trick
+def pairwisedist(locations):
+    c = np.transpose(locations[:, :, np.newaxis], (2,1,0))
+    c = locations[:, :, np.newaxis] - c
+    c = np.add.reduce(np.square(c), 1)
+    return np.sqrt(c)    
+
+def tsp(infile='tsp.txt'):
+    with open(infile, 'r') as f:
+        nvert = [int(x) for x in f.readline().split()]
+        lastx, lasty = 0.0, 0.0
+        locations = np.zeros((nvert, 2), dtype='float32')
+        bestval = np.zeros((2**nvert, nvert), dtype='float32')
+        dist = np.zeros((nvert, nvert), dtype='float32')
+        bestval[1:,1] = np.finfo(np.float32).max
+        bestval[0,1] = 1.0
+        for i, line in enumerate(f):
+            locations[i,] = [float(x) for x in line.split()]
+            bigfloat += distance(locations[i,], locations[i-1,])
+            # for j in xrange(i, nvert):
+            #     # bah, we should use real symmetric matrices to save space
+            #     d = distance(locations[i,], locations[j,])
+            #     dist[i,j] = d
+            #     dist[j,i] = d
+        
+
+        bestval[bestval == 0] = bigfloat
